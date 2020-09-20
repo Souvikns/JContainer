6555d9c31a59181c20297dd6dc29cfc9e724104f
Souvik <souvikde.ns@gmail.com>
2020-09-20 15:46:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,10 @@
-import fetch from 'isomorphic-unfetch'
-const Index = ({data}) => {
+const Index = () => {
 
     return <>
         <center>
             <h1>Create your own data storage service</h1>
-
-            {data.map(el => <div key={el._id}>
-            <p>{el.name}</p>
-            </div>)}
         </center>
     </>
-}
-
-export async function getServerSideProps(context){
-    
-    const res = await fetch('http://localhost:3000/api/container')
-    const data = await res.json()
-
-    return {
-        props: {
-            data: data
-        }
-    }
 }
 
 export default Index
