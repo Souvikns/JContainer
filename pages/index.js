c82371f9a00ab5a9a6c2b6031090bf56e7661d85
Souvikns <souvikde.ns@gmail.com>
2020-09-21 10:06:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,19 @@ import { Auth } from '../components/auth'
 const Index = ({data, isLoggedIn}) => {
 
     return <>
-    {((isLoggedIn)? null: <Auth />)}
+    {/* {((isLoggedIn)? null: <Auth />)} */}
     {data}
     </>
 }
 
-export async function getStaticProps(context){
+export async function getServerSideProps(context){
     const {check} = require('../lib/test')
     let data = check('Souvik')
 
-    let isLoggedIn = localStorage.getItem('isLoggedIn')
 
     return {
         props: {
-            data,
-            isLoggedIn
+            data
         }
     }
 }
