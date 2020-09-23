0267da8817dc2910e7c0189bbd7625ddd5743646
Souvikns <souvikde.ns@gmail.com>
2020-09-24 01:46:11 +0530

    added styling

--- components/app/api.jsx
+++ components/app/api.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+import {
+    Paper,
+    makeStyles,
+    Grid,
+    Button,
+    Typography,
+    Container
+} from '@material-ui/core'
+
+const useStyle = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(2, 4)
+    }
+}))
+
 const Api = props => {
     let api_key = localStorage.getItem('JC-api-key')
 
@@ -9,8 +24,37 @@ const Api = props => {
         setRev(!rev)
     }
 
+    const classes = useStyle()
+
     return <>
-        <p><strong>API Key:</strong> {((rev) ? api_key : "*********")} <button onClick={reveal}>{((rev) ? "hide" : "show")}</button></p>
+        <Container>
+            <Paper className={classes.root} variant="outlined">
+
+                <Grid container spacing={2}>
+                    <Grid item xs={3} />
+
+                    <Grid item xs={2}>
+                        <Typography>
+                            API Key
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={2}>
+                        <Typography>
+                            {((rev) ? api_key : "*********")}
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={2}>
+                        <Button onClick={reveal}>{((rev) ? "hide" : "show")}</Button>
+                    </Grid>
+
+                    <Grid item xs={3} />
+
+                </Grid>
+
+            </Paper>
+        </Container>
     </>
 }
 
