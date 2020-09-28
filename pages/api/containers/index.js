92784aadbb8de152607a1e5313604cfd807bc78c
Souvik <souvikde.ns@gmail.com>
2020-09-28 23:46:34 +0530

    Update index.js

--- pages/api/containers/index.js
+++ pages/api/containers/index.js
@@ -1,15 +1,29 @@
 import nextConnect from 'next-connect'
+import database from '../../../middlewares/db'
+import apikeyCheck from '../../../middlewares/apistring'
 
 const handler = nextConnect()
 
 
 handler
-    .post(async (req, res) => {
-        /**
-         * Create new continer
-         * and update _metadata collection
-         */
-        let { name } = req.body
-    })
+    .use(database)
+    .use(apikeyCheck)
+    /**
+     * To get the list of containers
+     */
+    .get()
+    /**
+     * To create a new container
+     */
+    .post()
+    /**
+     * To update a the name of a container
+     */
+    .patch()
+    /**
+     * To delete a container
+     */
+    .delete()
+
 
 export default handler
