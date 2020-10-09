ae83482fcc44ce412f96f37ab5890c9a3b073445
Souvik <souvikde.ns@gmail.com>
2020-10-09 23:36:53 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,19 +29,5 @@ const Index = () => {
     </>
 }
 
-export async function getStaticProps(context) {
-
-    const content = await import(`../markdown/index.md`)
-
-    const data = content.default
-
-
-    return {
-        props: {
-            data
-        }
-    }
-}
-
 
 export default Index
